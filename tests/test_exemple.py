52baf3f56ea4edde8bb11c07ae3aeffa6407f8b8
Florian Claux <159036680+fclaux@users.noreply.github.com>
2026-01-27 11:17:51 +0100

    Add tests system for SonarCloud

---
+++ tests/test_exemple.py
@@ -0,0 +1,8 @@
+# Fichier: tests/test_exemple.py
+
+def test_par_defaut():
+    """
+    Ce test ne fait rien d'autre que vérifier que True est égal à True.
+    Il sert à valider que la configuration Tox et Pytest fonctionne.
+    """
+    assert True
